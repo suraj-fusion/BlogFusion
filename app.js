60cb8de38487f7780dcebf9b951603e4581a0ee9
Suraj <surajrajgp@gmail.com>
2023-05-22 03:23:26 +0530

    added server port

--- app.js
+++ app.js
@@ -250,6 +250,6 @@ app.get("/posts/:path",function(req,res){
 
 
 //setting up server to listem on port 3000
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT ||3000, function() {
+  console.log("Server started ");
 });
